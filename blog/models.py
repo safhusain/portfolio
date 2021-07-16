71a5e1822caa40c95f9c2fc9d115cbb35d60fad9
shusain <shusain@manh.com>
2021-07-16 13:57:46 +0100

    Added Blogs models

--- blog/models.py
+++ blog/models.py
@@ -2,4 +2,6 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=50)
+    pub_date = models.DateTimeField()
+    last_date = models.DateTimeField(auto_now=True)
     summary = models.CharField(max_length=200)
